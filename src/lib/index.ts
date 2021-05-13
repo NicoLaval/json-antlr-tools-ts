b12ad9e7e66459c19c6af716ffa4c0529f564f10
NicoLaval <nicolas.laval@insee.fr>
2021-05-13 16:59:51 +0200

    Update

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -3,5 +3,7 @@ export { JSONParser as Parser } from "./generated/JSONParser";
 export { default as grammar } from "./generated/JSON.g4";
 
 const initialRule = "json";
-
 export { initialRule };
+
+const id = "json";
+export { id };
